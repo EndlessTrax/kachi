cbe1ba5a187b01a1c0479d4ac9c14497338f870c
Ricky White <ricky@rickywhite.net>
2024-11-01 18:02:18 -0400

    Bump version: v0.1.9

--- src/kachi/__init__.py
+++ src/kachi/__init__.py
@@ -2,7 +2,7 @@
 
 import logging
 
-__version__ = "0.1.8"
+__version__ = "0.1.9"
 
 logging.basicConfig(level=logging.INFO, format="%(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
